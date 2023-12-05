bd8bb9d12667b0c83359b54c4711e30f3b4d665d
justmoresmile <justmoresmile@yandex.ru>
2023-12-05 23:27:52 +0300

    05-js-v4

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -7,7 +7,7 @@ switch(true) {
         console.log('У меня 100 пудов все получится')
         break
     case rule.length == 25 :
-        console.log('«50 на 50»')
+        console.log('50 на 50')
        break
     default:
         console.log('Если долго мучаться, всеравно получиться')
