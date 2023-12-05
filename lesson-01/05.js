925f1431364005cb7d1a2b66cbffcf6d11335b59
justmoresmile <justmoresmile@yandex.ru>
2023-12-05 23:25:42 +0300

    05-js-v3

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -6,8 +6,9 @@ switch(true) {
     case rule.length > 25 :
         console.log('У меня 100 пудов все получится')
         break
-    case rule.length === 25 :
-    console.log('«50 на 50»')
-    break
-    
+    case rule.length == 25 :
+        console.log('«50 на 50»')
+       break
+    default:
+        console.log('Если долго мучаться, всеравно получиться')
 }
