e66b75bb1f5c3798aa69faf393c0d6ac56afee71
justmoresmile <justmoresmile@yandex.ru>
2023-12-17 21:48:32 +0300

    cloneAdnEditObject

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -3,3 +3,10 @@ let passport = {
     surname: "Petrov",
 };
 
+let clone = {}
+for(let key in passport)  {
+    clone[key] = passport[key]
+}
+clone.name = 'Ivan'
+console.log(passport.name);
+console.log(clone.name);
