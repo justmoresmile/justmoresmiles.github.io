0e5c826e2591ad330524853a96370d787200236e
justmoresmile <justmoresmile@yandex.ru>
2023-12-17 22:15:48 +0300

    married = true v1

---
+++ lesson-02/07.js
@@ -0,0 +1,14 @@
+let passportMarried = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "Bobryisk"
+    }
+};
+
+let clonePassportMarried = structuredClone(passportMarried)
+
+clonePassportMarried.married = "true";
+console.log(passportMarried);
+console.log(clonePassportMarried)
