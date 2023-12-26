91b2b949876045815d81f08c312e2c072cfaec7e
justmoresmile <justmoresmile@yandex.ru>
2023-12-26 22:26:09 +0300

    fruits(v1.4)

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,3 @@
 const  fruits = ["яблоко", "банан", "апельсин", "манго", "киви", "ананас", "виноград", "груша", "слива", "персик"];
-const fruitsSlice = fruits.slice(1, (fruits.length -1, fruits.length-2))
+const fruitsSlice = fruits.slice(1, 8)
 console.log(fruitsSlice);
