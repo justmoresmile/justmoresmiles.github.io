ef617ba563ca33e2758215055910a82653287acf
justmoresmile <justmoresmile@yandex.ru>
2023-12-26 20:42:49 +0300

    flagGermany(v1)

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,10 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    return colors.filter(color => {
+        if(color ===  "черный" || (color === "красный" || color ==="желтый")   )
+        return color
+    }).join('-')
+}
+
+console.log(createColorString());
