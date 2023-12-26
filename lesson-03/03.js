ffaae12669b389c34fb59a2033c90cabd4947a9b
justmoresmile <justmoresmile@yandex.ru>
2023-12-26 21:41:50 +0300

    frits(v1.3

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,2 +1,3 @@
 const  fruits = ["яблоко", "банан", "апельсин", "манго", "киви", "ананас", "виноград", "груша", "слива", "персик"];
 const fruitsSlice = fruits.slice(1, (fruits.length -1, fruits.length-2))
+console.log(fruitsSlice);
